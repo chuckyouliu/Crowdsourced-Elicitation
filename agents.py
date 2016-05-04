f03c59d1303f4d3481ca92eb6cc0108d38d38beb
Charles Liu <therealcharlesliu@gmail.com>
2016-05-04 03:06:06 -0400

    final paper for crowdsourcing

--- agents.py
+++ agents.py
@@ -1,18 +1,20 @@
 import numpy as np
 
 class FullEffortTrueAgent(object):
-    def __init__(self, types):
+    def __init__(self, types, min_prof = 0.5, max_prof = 1.0):
+        self.min_prof = min_prof
+        self.max_prof = max_prof
         self.reset(types)
             
     def reset(self, tasks):
-        self.tasks = tasks
+        self.tasks = dict(tasks)
         self.reports = {}
         self.reward = 0
         for idx in self.tasks:
             self.report(idx)
         
     def proficiency(self):
-        return np.random.uniform(.5, 1)
+        return np.random.uniform(self.min_prof, self.max_prof)
     
     def has_effort(self):
         return 1.0
@@ -45,7 +47,7 @@ class FullEffortTrueAgent(object):
                 if len(ref_nonoverlap) == d:
                     break
         if len(ref_nonoverlap) != d or len(my_nonoverlap) != d:
-            raise ValueError("Raters don't have {} non-overlapping tasks".format(d))
+            raise ValueError("Raters don't have {} non-overlapping tasks. Ref:{}, Agent:{}".format(d, ref_nonoverlap, my_nonoverlap))
             
         my_sum = np.sum(my_nonoverlap, dtype=np.float)
         ref_sum = np.sum(ref_nonoverlap, dtype=np.float)
@@ -53,8 +55,8 @@ class FullEffortTrueAgent(object):
         self.reward += reward_factor*reward
             
 class FullEffortFalseAgent(FullEffortTrueAgent):
-    def __init__(self, types):
-        super(FullEffortFalseAgent, self).__init__(types)
+    def __init__(self, types, min_prof = 0.5, max_prof = 1.0):
+        super(FullEffortFalseAgent, self).__init__(types, min_prof, max_prof)
         
     def give_signal(self):
         return 0.0
@@ -67,9 +69,27 @@ class NoEffortAgent(FullEffortTrueAgent):
         return 0.0
     
 class RandomEffortTrueAgent(FullEffortTrueAgent):
-    def __init__(self, types, max_effort=1.):
+    def __init__(self, types, min_prof = 0.5, max_prof = 1.0, max_effort = 1.0):
         self.max_effort = max_effort
-        super(RandomEffortTrueAgent, self).__init__(types)
+        super(RandomEffortTrueAgent, self).__init__(types, min_prof, max_prof)
         
     def has_effort(self):
         return np.random.uniform(0, self.max_effort)
+    
+class VariableEffortTrueAgent(FullEffortTrueAgent):
+    def __init__(self, types, costfunc, effort, min_prof = 0.5, max_prof = 1.0):
+        self.effort = effort
+        self.costfunc = costfunc
+        super(VariableEffortTrueAgent, self).__init__(types, min_prof, max_prof)
+        
+    def proficiency(self):
+        return self.min_prof + self.effort*(self.max_prof - self.min_prof)
+        
+    def has_effort(self):
+        return 1.0
+    
+    def inc_effort(self, size):
+        self.effort = max(min(self.effort+size, 1.0), 0.0)
+
+    def incur_cost(self):
+        self.reward -= self.costfunc(self.effort)
