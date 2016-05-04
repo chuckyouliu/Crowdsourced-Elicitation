86ca6ce2367cede5bfc8229ec2cb703f379ca176
Virgile Audi <vaudi@g.harvard.edu>
2016-05-04 19:19:11 -0400

    final

--- PP.py
+++ PP.py
@@ -2,20 +2,49 @@ import numpy as np
 
 class agent_pp:
 
-    def __init__(self, prior, sig_dist):
+    def __init__(self, prior_type, sig_dist):
 
-        self.prior  = prior 
+        self.prior_type  = prior_type 
         self.sig_dist = sig_dist
-        self.ntype = len(prior)
-        self.post = None
+        self.ntype = len(prior_type)
+        self.nsignal = len(sig_dist[0])
 
-    def evaluate_post(self):
-
-        post = np.zeros(self.ntype)
-        for i in range(self.ntype):
-            post[i] = np.sum(self.prior * self.sig_dist[:,i])
+        prior_signal = np.zeros(self.nsignal)
+        for i in range(self.nsignal):
+            prior_signal[i] = np.sum(self.prior_type * self.sig_dist[:,i])
         
-        self.post = post
+        self.prior_signal = prior_signal
+
+        type_given_signal = np.zeros((self.nsignal, self.ntype))
+        for i in range(self.nsignal):
+            for j in range(self.ntype):
+                type_given_signal[i,j] = self.sig_dist[j][i]*self.prior_type[j]/self.prior_signal[i]
+        self.type_given_signal = type_given_signal
+
+    # def evaluate_prior_signal(self):
+
+    #     prior_signal = np.zeros(self.ntype)
+    #     for i in range(self.ntype):
+    #         prior_signal[i] = np.sum(self.prior_type * self.sig_dist[:,i])
+        
+    #     self.prior_signal = prior_signal
+
+    # def evaluate_type_given_signal(self):
+
+    #     p1 = self.sig_dist[0][0]*self.prior_type[0]/self.prior_signal[0]
+    #     p2 = self.sig_dist[0][1]*self.prior_type[0]/self.prior_signal[1]
+    #     self.type_given_signal = np.array([[p1,1-p1],[p2,1-p2]])
+
+    def signal_given_signal(self, observed, other):
+        p = 0
+        for i in range(self.ntype):
+            p += self.type_given_signal[observed][i]*self.sig_dist[i][other]
+        return p
+
+    def update_prior_signal(self, observation):
+        for i in range(self.nsignal):
+            self.prior_signal[i] = self.signal_given_signal(observation, i)
+        
 
     def prob_given_report(self, ref, self_report, ref_report):
 
@@ -43,6 +72,11 @@ class agent_pp:
             l.remove(i)
             return np.random.choice(l)
 
+def binary_quadratic(ag_rep, ref_rep):
+    if ref_rep == 0:
+        return 2*ag_rep - ag_rep**2
+    else:
+        return 1 - ag_rep**2
 
 # class scoring_rule:
 
@@ -71,6 +105,16 @@ def spherical(a1,a1_rep,a2, a2_rep):
 def logarithmic(a1,a1_rep,a2, a2_rep):
 
     return np.log(a1.prob_given_report(a2, a1_rep, a2_rep))
+
+
+def infer_report(prev, new):
+    if prev>new:
+        return 1
+    else:
+        return 0
+
+def reward_bpp(prev, new, implicit_ref):
+    return binary_quadratic(prev, implicit_ref) + binary_quadratic(new, implicit_ref)
 
 # def set_score(self, type_):
 
